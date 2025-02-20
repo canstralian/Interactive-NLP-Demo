2b8d5808a190d0d666a37f212b7dc133944c4ac0
Esteban Cara de Sexo <8595080+canstralian@users.noreply.github.com>
2025-02-20 06:06:02 +0000

    Initial commit

---
+++ main.sh
@@ -0,0 +1,12 @@
+python3 -m venv .venv
+   source .venv/bin/activate  # On Windows: .venv\Scripts\activate
+
+pip install streamlit
+   # Install any other dependencies your app requires.  For example:
+   # pip install numpy pandas requests
+
+streamlit run app.py
+
+# Sources:
+# 1. https://github.com/AmarjeetMohanty/Streamlit-Hackathon
+# 2. https://github.com/sivkri/bam2bai
